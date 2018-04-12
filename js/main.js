1ecd306bab02aa80afc70343ee69375792e61bca
mwatson <mattwat@gmail.com>
2018-04-12 19:27:20 -0400

    add config object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let config;
 let paused = true;
 let showVectorField = true;
 let showParticles = true;
@@ -9,15 +10,10 @@ let plane;
 let f;
 
 let vectorField = function(x, y) {
-	let vec;
-	vec = {
-		'x': y**3 - 9*y,
-		'y': x**3 - 9*x
-	}
-	// vec = {
-	// 	'x': 5,
-	// 	'y': 10
-	// }
+	let vec = {};
+	vec = { 'x': y**3 - 9*y, 'y': x**3 - 9*x }
+	// vec = { 'x': -y, 'y': x }
+	// vec = { 'x': x, 'y': y }
 	return vec
 }
 
@@ -26,9 +22,31 @@ let viewport = {
 	'y': { 'min': -zoom, 'max': zoom }
 }
 
-
 function setup() {
-	// createCanvas(600, 600);
+	config = {
+		'plane': {
+			'grid': {
+				'color': color(128, 128, 128, 255),
+				'weight': 1,
+				'axis': {
+					'tickmarkSize': 10,
+					'color': color(128, 128, 128, 255),
+					'weight': 4,
+				}
+			},
+			'friction': 0.001
+		},
+		'vector': {
+			'spacing': 0.5,
+			'color': color(0, 255, 255, 64),
+			'weight': 2,
+		},
+		'particle': {
+			'spacing': 0.25,
+			'diameter': 8,
+			'color': color(220, 255, 220),
+		},
+	};
 	createCanvas(windowWidth, windowHeight);
 	plane = new Plane();
 	system = new ParticleSystem();
