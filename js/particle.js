c7971d7b7db6d4ce0f00a0a7208b9ccbb83b7459
mwatson <mattwat@gmail.com>
2018-04-12 23:36:53 -0400

    grow particle on spawn

--- js/particle.js
+++ js/particle.js
@@ -1,16 +1,23 @@
 class Particle {
 	constructor(x, y) {
-		this.diameter = config.particle.diameter;
+		this.diameter = 0;
 		this.x = x;
 		this.y = y;
 		this.dx = 0;
 		this.dy = 0;
 		this.color = config.particle.color;
+		this.new = true;
 	}
 	draw() {
 		noStroke();
 		// fill(color(255 - this.color, this.color, this.color));
 		fill(this.color);
+		if (this.new) {
+			this.diameter += 1;
+			if (this.diameter == config.particle.diameter) {
+				this.new = false;
+			}
+		}
 		ellipse(this.x * width / (viewport.x.max - viewport.x.min), -this.y * width / (viewport.x.max - viewport.x.min), this.diameter, this.diameter);
 		// text(`(${this.x.toFixed(2)},${this.y.toFixed(2)})`, this.x * width / (viewport.x.max - viewport.x.min), -this.y * width / (viewport.x.max - viewport.x.min));
 	}
